eb9f171bc044526b3b2c69c1715b08799e64620e
Ivan De Martino <ivan_demartino@outlook.com>
2020-12-04 22:57:14 -0300

    Add HexColor converter

--- DemarteCore/Sources/DemarteCore/DemarteCore.swift
+++ DemarteCore/Sources/DemarteCore/DemarteCore.swift
@@ -1,3 +1,21 @@
-struct DemarteCore {
-    var text = "Hello, World!"
+import UIKit
+
+class DemarteCore {
+
+  class func colorFromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
+    let rComponent, gComponent, bComponent: CGFloat
+    let offset = hexString.hasPrefix("#") ? 1 : 0
+    let start = hexString.index(hexString.startIndex, offsetBy: offset)
+    let hexColor = String(hexString[start...])
+    let scanner = Scanner(string: hexColor)
+    var hexNumber: UInt64 = .zero
+
+    if scanner.scanHexInt64(&hexNumber) {
+      rComponent = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+      gComponent = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+      bComponent = CGFloat(hexNumber & 0x0000ff) / 255
+      return UIColor(red: rComponent, green: gComponent, blue: bComponent, alpha: alpha)
+    }
+    return UIColor(red: .zero, green: .zero, blue: .zero, alpha: alpha)
+  }
 }
--- DemarteCore/Tests/DemarteCoreTests/DemarteCoreTests.swift
+++ DemarteCore/Tests/DemarteCoreTests/DemarteCoreTests.swift
@@ -2,14 +2,13 @@ import XCTest
 @testable import DemarteCore
 
 final class DemarteCoreTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DemarteCore().text, "Hello, World!")
-    }
+
+  func testColorRedEqual() {
+    let color = DemarteCore.colorFromHexString("FF0000")
+    XCTAssertEqual(color, .red)
+  }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testColorRedEqual", testColorRedEqual),
     ]
 }
